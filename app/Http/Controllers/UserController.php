bfdeebc78d30746839fcf810e6a4e9f2351ffc2d
Bi5cuitOliva <marlonjojo83@gmail.com>
2024-11-20 01:15:00 +0300

    created a store function which is going to validate the user fields for name,email,password  and the roles that they can be assigned.'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -20,5 +21,25 @@ class UserController extends Controller
     public function create()
     {
         return view('role-permission.user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|max:255|unique:users,email',
+            'password'=>'required|string|min:8|max:20',
+            'roles'=>'required'
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ]);
+
+        $user->syncRoles($request->roles);
+
+        return redirect('/users')->with('status','User created successfully with entitlements');
     }
 }
--- resources/views/role-permission/nav-links.blade.php
+++ resources/views/role-permission/nav-links.blade.php
@@ -1,6 +1,5 @@
 <div class="container mt-3">
-    <a href="{{url('roles')}}" class="btn btn-primary mx-2">Roles</a>
-    <a href="{{url('permissions')}}" class="btn btn-info mx-2">Permissions</a>
+    <a href="{{url('roles')}}" class="btn btn-primary mx-2">Responsibilities</a>
+    <a href="{{url('permissions')}}" class="btn btn-info mx-2">Entitlement</a>
     <a href="{{url('users')}}" class="btn btn-warning mx-2">Users</a>
 </div>
- 
