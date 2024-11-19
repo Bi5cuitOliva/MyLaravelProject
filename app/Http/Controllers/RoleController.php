31e2931bacfe98a69fb3558f070973112af2ce47
Bi5cuitOliva <marlonjojo83@gmail.com>
2024-11-19 14:47:51 +0300

    Added the methods and functionality for role controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    public function index(){
+        $roles = Role::get();
+        return view('role-permission.role.index ', [
+            'roles' => $roles
+        ]);
+    }
+
+    public function create(){
+           return view('role-permission.role.create');
+    }
+
+    public function store(Request $request){
+         $request->validate([
+            'name' =>[
+                'required',
+                'string',
+                'unique:roles,name'
+            ]
+            ]);
+      Role::create([
+         'name' => $request->name
+      ]);
+        return redirect('roles') ->with('status','Responsibility Confirmed Successfully');
+    }
+
+    public function edit(Role $role){
+         return view('role-permission.role.edit',[
+            'role'=> $role
+         ]);
+    }
+
+    public function update(Request $request, Role $role){
+
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'unique:roles,name,'.$role->id
+            ]
+
+            ]);
+
+            $role->update([
+                'name' => $request->name
+            ]);
+
+            return redirect('roles')->with('status','Responsibility Modified Successfully');
+
+    }
+
+    public function destroy($roleId){
+        $role =Role::find($roleId);
+        $role->delete();
+        return redirect('roles')->with('status', 'Responsibility Deleted Successfully');
+    }
+}
